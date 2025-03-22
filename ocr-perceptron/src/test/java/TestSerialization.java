8a8c052542578b8f963b52b221438ccf47fe5a88
Cole Johnson <coledjohn@gmail.com>
2025-03-22 16:20:13 -0600

    adding Serialization unit test stuff

--- ocr-perceptron/src/test/java/TestSerialization.java
+++ ocr-perceptron/src/test/java/TestSerialization.java
@@ -1,2 +1,31 @@
+import com.perceptron.nn.ActivationFunction;
+import com.perceptron.nn.CostFunction;
+import com.perceptron.nn.Layer;
+import com.perceptron.nn.NeuralNetwork;
+import com.perceptron.test_train.DataFrame;
+import com.perceptron.test_train.ResourceManager;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 public class TestSerialization {
+    @Test
+    public void testSerialization() {
+        // input layer can hold input as activations
+        Layer input = new Layer(28*28, ActivationFunction.SIGMOID);
+        // new cool hidden layer
+        Layer hidden = new Layer(30, ActivationFunction.SIGMOID);
+        // output holds predictions as activations
+        Layer output = new Layer(10, ActivationFunction.SIGMOID);
+        // create new nn based on config
+        NeuralNetwork nn = new NeuralNetwork(CostFunction.MSE, input, hidden, output);
+        // instantiate resource manager to hand us a dataframes (wasteful, but hey it's just a test)
+        DataFrame df = ResourceManager.getTrainingData().next();
+        // now make very wrong prediction based on initial nn config
+        double[] p = nn.prediction(df);
+        System.out.println(Arrays.toString(p));
+        // serialize nn, then make prediction again,
+        // if serialization interface works it should be the same
+
+    }
 }
