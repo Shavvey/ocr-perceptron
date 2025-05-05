d77ee8559812bee7353a70cb325a5f6a19985d90
Cole Johnson <coledjohn@gmail.com>
2025-05-04 20:57:28 -0600

    second model for realsies

--- ocr-perceptron/src/main/java/com/perceptron/main/Main.java
+++ ocr-perceptron/src/main/java/com/perceptron/main/Main.java
@@ -1,16 +1,29 @@
 package com.perceptron.main;
 
-import com.perceptron.gui.GUI;
+import com.perceptron.nn.ActivationFunction;
+import com.perceptron.nn.CostFunction;
+import com.perceptron.nn.Layer;
+import com.perceptron.nn.NeuralNetwork;
 
-import javax.swing.*;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new GUI();
-            frame.setVisible(true);
-        });
+        // create different layers, with different layer count and ActivationFunction
+        Layer output = new Layer(10, ActivationFunction.SIGMOID);
+        Layer hidden1 = new Layer(30, ActivationFunction.SIGMOID);
+        Layer hidden2 = new Layer(30, ActivationFunction.SIGMOID);
+        Layer input = new Layer(28 * 28, ActivationFunction.SIGMOID);
+        // configure the network and create it
+        NeuralNetwork nn = new NeuralNetwork(CostFunction.MSE, input, hidden1, hidden2, output);
+        nn.display();
+        nn.train(0.30, 5, 10);
+        nn.test();
+        Scanner scan = new Scanner(System.in);
+        if (scan.nextLine().equals("yes")) {
+            nn.serialize("model-3");
+        }
 
     }
 }
